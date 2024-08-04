430e8b03bb780af7619c01c00e56d2ba49a7dbe3
Konstantin Pechenenko <kpechenenko@yandex.ru>
2024-08-05 02:51:22 +0700

    add draft of hw8

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,41 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for k, v := range env {
+		// Нужно удалить каждый элемент из списка. Часть элементов после удаления нужно присвоить заново
+		if err := os.Unsetenv(k); err != nil {
+			return 1
+		}
+		// у переменных, которые нужно присвоить, флаг в читающей функции записан в false
+		// при этом по v.Value != "" сравнивать нельзя, т.к. в value может быть пустая строка
+		if !v.NeedRemove {
+			if err := os.Setenv(k, v.Value); err != nil {
+				return 1
+			}
+		}
+	}
+	var c *exec.Cmd
+	if len(cmd) > 1 {
+		c = exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	} else {
+		c = exec.Command(cmd[0]) //nolint:gosec
+	}
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	c.Stdin = os.Stdin
+	if err := c.Run(); err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode()
+		}
+		return 1
+	}
+	return 0
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+	if len(args) < 3 {
+		log.Println("Usage: go-envdir dir command [args]")
+		os.Exit(1)
+	}
+	// log.Printf("args: %v\n", args)
+	env, err := ReadDir(args[1])
+	if err != nil {
+		log.Printf("Error reading env from dir %s: %s\n", args[1], err)
+		os.Exit(1)
+	}
+	log.Printf("readed env: %+v\n", env)
+	exitCode := RunCmd(args[2:], env)
+	if exitCode != 0 {
+		log.Println("Error running command.")
+	}
+	os.Exit(exitCode)
 }
