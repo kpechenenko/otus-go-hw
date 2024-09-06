1583120f6b5e8ef6bd7de5bbddfbdd7648761a22
Konstantin Pechenenko <kpechenenko@yandex.ru>
2024-09-06 13:32:20 +0700

    Revert "tmp res"
    This reverts commit 34f979b0e7130318adb00925d144e164f0cfaf85.

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"io"
-	"net"
 	"time"
 )
 
@@ -15,58 +13,9 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	return &telnetClient{
-		address: address,
-		timeout: timeout,
-		in:      in,
-		out:     out,
-	}
-}
-
-type telnetClient struct {
-	address string
-	timeout time.Duration
-	in      io.ReadCloser
-	out     io.Writer
-	conn    net.Conn
-}
-
-func (c *telnetClient) Connect() error {
-	var err error
-	if c.conn, err = net.DialTimeout("tcp", c.address, c.timeout); err != nil {
-		return err
-	}
+	// Place your code here.
 	return nil
 }
 
-func (c *telnetClient) Close() error {
-	return c.conn.Close()
-}
-
-func (c *telnetClient) Send() error {
-	scanner := bufio.NewScanner(c.in)
-	for scanner.Scan() {
-		b := scanner.Bytes()
-		if _, err := c.conn.Write(b); err != nil {
-			return err
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (c *telnetClient) Receive() error {
-	scanner := bufio.NewScanner(c.conn)
-	for scanner.Scan() {
-		b := scanner.Bytes()
-		if _, err := c.out.Write(b); err != nil {
-			return err
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-	return nil
-}
+// Place your code here.
+// P.S. Author's solution takes no more than 50 lines.
